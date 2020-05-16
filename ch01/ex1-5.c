6376232d3baab6541e2194003e2435d197e484d9
Prabhakar Patil <prpatil12@gmail.com>
2020-05-16 11:14:48 +0530

    Exercise 1-5

---
+++ ch01/ex1-5.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* print Fahreneit-Celsius table in reverse order
+ * for fahr = 300, 280, ..., 0*/
+
+main()
+{
+	int fahr;
+	printf ("Fahreneit Celsius\n");
+	for(fahr = 300; fahr >= 0; fahr = fahr - 20)
+		printf ("%3d\t%6.1f\n", fahr, (5.0/9.0)*(fahr-32.0));
+}
