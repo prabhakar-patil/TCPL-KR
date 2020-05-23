e80a751f0c721553037e74401eb3df68a19fdf8b
Prabhakar Patil <prpatil12@gmail.com>
2020-05-23 11:52:01 +0530

    Exercise 1-12. (Refactored) Write a program that prints its input one word per line.

--- ch01/ex1-12.c
+++ ch01/ex1-12.c
@@ -1,30 +1,22 @@
 /* Write a program that prints its input one word per line */
 #include <stdio.h>
 
-#define IN 1 /*Inside a word*/
-#define OUT 0 /*Outside a word*/
-
 main()
 {
 	int c;
 	int state;
 	int is_prev_nl = 0;  /* 1: if previous character is newline otherwise 0*/
-	state = OUT;
 
 	while((c = getchar()) != EOF) {
-		if(c == ' ' || c == '\t' || c == '\n')
-			state = OUT;
-		else {
-			putchar(c);
-			is_prev_nl = 0;
-		}
-
-		if(state == OUT) {
-			state = IN;
+		if(c == ' ' || c == '\t' || c == '\n') {
 			if(is_prev_nl == 0) {
 				putchar('\n');
 				is_prev_nl = 1;
 			}
+		}
+		else {
+			putchar(c);
+			is_prev_nl = 0;
 		}
 	}
 }
