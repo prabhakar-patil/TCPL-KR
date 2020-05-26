3e255c856d03ca5b2fee835eae93e78efc72ceed
Prabhakar Patil <prpatil12@gmail.com>
2020-05-26 21:53:09 +0530

    Exercise 1-23. (Improved) Write a program to remove all comments from a C program. Don't forget to handle quoted strings and character constants properly. C comments don't nest.

--- ch01/ex1-23.c
+++ ch01/ex1-23.c
@@ -1,41 +1,58 @@
 /* Exercise 1-23. Write a program to remove all comments from a C program. Don't forget to handle quoted strings and character constants properly. C comments don't nest. */
 #include <stdio.h>
 
-#define MAXLINE 1000
-
-#define OUT 0	/* out of quoted string or comment */
-#define IN 1	/* in quoted string or comment */
+void skip_comment(int c);
+void print_quote(int c);
 
 main()
 {
-	int c, prev_c, pprev_c;
-	int quote;	/* hold state "IN/OUT" */
-	int comment;	/* hold state "IN/OUT" */
-	
-	quote = OUT;
-	comment = OUT;		
-	prev_c = 0;
-	pprev_c = 0;
+	int c, d;
 
 	while ((c = getchar()) != EOF) {
-		if (c == '"' && comment == OUT) {
-			if (quote == OUT)
-				quote = IN;
-			else 
-				quote = OUT;
+		if (c == '/') {
+			if ((d = getchar()) == '*')
+				skip_comment(c);
+			else {
+				putchar(c);
+				putchar(d);
+			}
 		}
-
-		if (c == '*' && prev_c == '/' && quote == OUT)
-			comment = IN;
-		if (c == '/' && prev_c == '*' && quote == OUT && comment == IN)
-			comment = OUT;
-		else if (comment == OUT && c!='/') {
-			if (c!='*' && prev_c=='/' && pprev_c != '*')
-				putchar('/');
+		else if (c == '"' || c == '\'') {
+			print_quote(c);
+		}
+		else {
 			putchar(c);
 		}
-		
-		pprev_c = prev_c;
-		prev_c = c;
 	}
+}
+
+/* removes comment from output prints */
+void skip_comment(int c)
+{
+	int d;
+	c = getchar();
+	d = getchar();
+
+	while (c != '*' || d != '/') {
+		c = d;
+		d = getchar();
+	}
+}
+
+/* print quotes separately, since this is specail case */
+void print_quote(int c)
+{
+	int d;
+
+	d = 0;
+	putchar(c); /* print starting quoute */
+
+	while ((d = getchar()) != c) {
+		putchar(d);
+		if (d == '\\') {/* escape sequence inside quote */
+			d = getchar();
+			putchar(d);	/* print escaped character */
+		}
+	}
+	putchar(d); /* print ending quote */
 }
